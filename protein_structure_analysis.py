44b6d9d38aed518fe1623945c665d31eae9de5f7
Ryusuke_Morita <r.frtun702@gmail.com>
2022-01-11 23:39:01 +0900

    Update

--- protein_structure_analysis.py
+++ protein_structure_analysis.py
@@ -8,3 +8,8 @@ for pdb_id in pdb_ids:
     pdbl.retrieve_pdb_file(pdb_id, pdir="pdb_files/", file_format="pdb", overwrite=True)
 
 pdbl.download_pdf_files(pdb_ids, pdir="pdb_files/", file_fomat="pdb" , overwrite=True)
+
+pdbl = PDBList()
+parser = PDBParser(QUIET = True)
+pdbl.retrieve_pdb_file("1EN2", pdir="./", file_format="pdb", overwrite=True)
+struc = parser.get_structure("1EN2", "pdb1en2.ent")
